d9b7d0a51ff3ec5e471bcf5b981a780f95e4bb96
chethankswamy <chethan@acclivitysoftware.com>
2014-11-17 11:28:46 +0530

    First commit

---
+++ uploads/gateway_ach.php
@@ -0,0 +1,15 @@
+<?php
+	defined('SYSPATH') OR die('No direct access allowed.');
+	
+	/**
+	*Contains configuration variables replated to ach gateway
+	*/
+	
+	// array containing ACH sandbox credentilas 
+	$config['sandobx_creds'] = array(
+								  'ach_gateway_id'			=> '132745',
+								  'ach_gateway_password'	=> 'tuMcRs59',
+								  'apli_login_id'			=> 'cGNVYACJ5T',
+								  'transaction_key'			=> 'nYeilhxm29Ho51s'
+								 );
+?>
